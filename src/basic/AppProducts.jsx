a94b7757f19c98a2c07ddd74d601cec69687e743
song <55rsearchco@gmail.com>
2023-02-18 14:52:23 +0900

    마우스 따라 가기 (마우스 이벤트)

--- src/basic/AppProducts.jsx
+++ src/basic/AppProducts.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { useState } from 'react';
 import Products from './components/Products';
 
 export default function AppProducts() {
--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Products() {
   const [products, setProducts] = useState([]);
