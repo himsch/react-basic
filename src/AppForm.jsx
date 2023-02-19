43c092a4b41d6903fe9bcf365e84e5abb629dc4d
song <55rsearchco@gmail.com>
2023-02-19 17:00:56 +0900

    Form에서 input 데이터를 컨트롤하기 (2. 하나의 객체 state)

--- src/AppForm.jsx
+++ src/AppForm.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 
 export default function AppForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [form, setForm] = useState({ name: '', email: '' });
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
   const handleSubmit = e => {
     e.preventDefault();
+    console.log(form);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -13,20 +17,16 @@ export default function AppForm() {
         type="text"
         id="name"
         name="name"
-        value={name}
-        onChange={e => {
-          setName(e.target.value);
-        }}
+        value={form.name}
+        onChange={handleChange}
       />
       <label htmlFor="email">이메일:</label>
       <input
         type="email"
         id="email"
         name="email"
-        value={email}
-        onChange={e => {
-          setEmail(e.target.value);
-        }}
+        value={form.email}
+        onChange={handleChange}
       />
       <button>Submit</button>
     </form>
