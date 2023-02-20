4e60838e7dd6ff762f2bfc74596cb2e756eae7b7
song <55rsearchco@gmail.com>
2023-02-20 11:17:51 +0900

    커스텀 훅 만들어 로직 재사용 해보자

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,29 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import useProducts from '../../hooks/use-products';
 
 export default function Products() {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(undefined);
-  const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
+  const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked(prev => !prev);
-
-  useEffect(() => {
-    // 초기화.
-    setLoading(true);
-    setError(undefined);
-
-    fetch(`data/${checked ? 'sale_' : ''}products.json`)
-      .then(res => res.json())
-      .then(data => {
-        console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
-        setProducts(data);
-      })
-      .catch(e => setError('에러가 발생함!!'))
-      .finally(() => setLoading(false));
-    return () => {
-      console.log('깨끗하게 청소하는 함수');
-    };
-  }, [checked]);
 
   if (loading) return <p>Loading</p>;
 
